e0394da65c9ceadfb2038fcca1b241a526af82c5
Pau111111 <paugarciapellicer96@gmail.com>
2018-03-16 17:08:14 +0100

    HomeActivity DIALOG with all the components and NEW APERANCE

--- app/src/main/java/com/adades/findyourphone/entities/PhoneListUser.java
+++ app/src/main/java/com/adades/findyourphone/entities/PhoneListUser.java
@@ -2,19 +2,14 @@ package com.adades.findyourphone.entities;
 
 import android.app.Activity;
 import android.graphics.drawable.BitmapDrawable;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.adades.findyourphone.AdminActivity;
-import com.adades.findyourphone.HomeActivity;
 import com.adades.findyourphone.R;
 import com.google.firebase.database.DatabaseReference;
 import com.squareup.picasso.Picasso;
@@ -44,7 +39,7 @@ public class PhoneListUser extends ArrayAdapter<Phone> {
         final TextView txtPhoneName = listViewItem.findViewById(R.id.namePhoneUser);
         final ImageView imageViewPhone = listViewItem.findViewById(R.id.imagePhoneUser);
 
-        Phone phone = phoneList.get(pos);
+        final Phone phone = phoneList.get(pos);
         txtPhoneName.setText(phone.getName());
         Picasso.get().load(phone.getImageUri()).into(imageViewPhone);
 
@@ -55,9 +50,17 @@ public class PhoneListUser extends ArrayAdapter<Phone> {
                 View mView = LayoutInflater.from(context).inflate(R.layout.dialog_show_phone_user,null);
                 TextView phoneName = mView.findViewById(R.id.phoneNameDialog);
                 ImageView ivPhone = mView.findViewById(R.id.ivDialog);
+                TextView os = mView.findViewById(R.id.osDialogUser);
+                TextView ram = mView.findViewById(R.id.ramDialogUser);
+                TextView cpu = mView.findViewById(R.id.cpuDialogUser);
+                TextView storage = mView.findViewById(R.id.storageDialogUser);
+
                 phoneName.setText(txtPhoneName.getText());
                 ivPhone.setImageBitmap(((BitmapDrawable) imageViewPhone.getDrawable()).getBitmap());
-
+                os.setText(phone.getSo());
+                ram.setText(phone.getRam());
+                cpu.setText(phone.getCpu());
+                storage.setText(phone.getStorage());
 
                 mbuilder.setView(mView);
                 AlertDialog dialog = mbuilder.create();
